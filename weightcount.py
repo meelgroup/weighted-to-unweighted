96296f47bd4e38fc01d5b591a91416cac68037fc
Mate Soos <soos.mate@gmail.com>
2019-12-18 23:17:00 +0100

    Cleaning up the code a bit

--- weightcount.py
+++ weightcount.py
@@ -32,19 +32,21 @@ import os
 import math
 import time
 import argparse
-LOGDIR = 'Log'
 
 
 def ProcessLine(line, originalVars, variable):
-    fields = line.strip().split()[:-1]
+    fields = line.strip().split()
+    assert fields[-1].strip() == "0", "All CNF lines must end with a 0!"
+
     finalLine = ''
-    for field in fields:
+    for field in fields[:-1]:
         sign = int(field)/abs(int(field))
-        if (field == '1'):
+        if field == '1':
             finalLine += str(sign*variable)+' '
         else:
             finalLine += str(sign*(originalVars+abs(int(field))-1))+' '
     finalLine += '0\n'
+
     return finalLine
 
 
@@ -61,9 +63,9 @@ def getCNF(variable, binStr, sign, origTotalVars):
     cnfClauses.append([binLen+1+origTotalVars])
     for i in range(binLen):
         newVar = binLen-i+origTotalVars
-        if (sign == False):
+        if sign is False:
             newVar = -1*(binLen-i+origTotalVars)
-        if (binStr[binLen-i-1] == '0'):
+        if binStr[binLen-i-1] == '0':
             cnfClauses.append([newVar])
         else:
             cnfClauses = pushVar(newVar, cnfClauses)
@@ -84,7 +86,7 @@ def EncodeCNF(variable, kWeight, iWeight, origtotalVars, origtotalClaus, indepen
         independentSet[origtotalVars+i+2] = 1
     complementStr = ''
     for i in range(len(binStr)):
-        if (binStr[i] == '0'):
+        if binStr[i] == '0':
             complementStr += '1'
         else:
             complementStr += '0'
@@ -98,7 +100,7 @@ def EncodeCNF(variable, kWeight, iWeight, origtotalVars, origtotalClaus, indepen
     currentVar = -variable
     cnfClauses = getCNF(variable, complementStr, False, origtotalVars)
     for i in range(len(cnfClauses)):
-        if (cnfClauses[i] in origCNFClauses):
+        if cnfClauses[i] in origCNFClauses:
             continue
         totalClaus += 1
         for j in range(len(cnfClauses[i])):
@@ -108,32 +110,27 @@ def EncodeCNF(variable, kWeight, iWeight, origtotalVars, origtotalClaus, indepen
 
 
 def ParseWeights(initWeight, precision):
-    if (initWeight == 1):
+    if initWeight == 1:
         return 1, 0
     weight = math.ceil(initWeight*pow(2, precision))
     while (weight % 2 == 0):
         weight = weight/2
         precision -= 1
     return weight, precision
-#  @tempCNFFile is temporary file.
+
+
 #  The code is straightforward chain formula implementation
-#
+def Transform(inputFile, outputFile, precision, runIndex):
+    # read in input CNF
+    with open(inputFile, 'r') as f:
+        lines = f.readlines()
 
-
-def Transform(inputFile, outputFile, tempCNFFile, precision, runIndex):
-    f = open(inputFile, 'r')
-    lines = f.readlines()
-    f.close()
     writeLines = ''
-    weightLine = ''
     independentSet = {}
     totalVars = 0
     origTotalVars = 0
     origTotalClaus = 0
     totalClaus = 0
-    indWeight = {}
-    origWeight = {}
-    minWeight = 0
     for line in lines:
         if (line.strip()[:2] == 'p '):
             fields = line.strip().split()
@@ -142,14 +139,17 @@ def Transform(inputFile, outputFile, tempCNFFile, precision, runIndex):
             origTotalVars = totalVars
             origTotalClaus = totalClaus
             continue
-        if (line.strip()[0].isdigit() or line.strip()[0] == 'c' or line.strip()[0] == '-'):
+        if line.strip()[0].isdigit() or line.strip()[0] == 'c' or line.strip()[0] == '-':
             writeLines += str(line)
+
+    # TODO: parse sampling set up above and use that instead
     for i in range(1, totalVars+1):
         independentSet[i] = 1
-    f = open(tempCNFFile, 'w')
-    f.write('p cnf '+str(totalVars)+' '+str(totalClaus)+'\n')
-    f.write(writeLines)
-    f.close()
+
+    origWeight = {}
+    indWeight = {}
+    minWeight = 0
+    weightLine = ''
     equalWeightVariables = 0
     for line in lines:
         if (line.strip()[:2] == 'w '):
@@ -175,7 +175,7 @@ def Transform(inputFile, outputFile, tempCNFFile, precision, runIndex):
                     equalWeightVariables += 1
                 indWeight[variable] = 1
             writeLines += weightLine
-    indWtWrteStr = ''
+
     indWriteStr = 'c ind '
     count = 0
     wtVarCount = 0
@@ -184,10 +184,11 @@ def Transform(inputFile, outputFile, tempCNFFile, precision, runIndex):
     origWtStr = ''
     for key in origWeight.keys():
         origWtStr += 'c o '+str(key)+' '+str(origWeight[key])+'\n'
-    f = open(outputFile, 'w')
-    f.write('p cnf '+str(totalVars)+' '+str(totalClaus)+' \n')
-    f.write(writeLines)
-    f.close()
+
+    with open(outputFile, 'w') as f:
+        f.write('p cnf '+str(totalVars)+' '+str(totalClaus)+' \n')
+        f.write(writeLines)
+
     return wtVarCount, origTotalVars, origTotalClaus, totalVars, totalClaus, minWeight, equalWeightVariables
 
 
@@ -199,70 +200,58 @@ def ensureDirectory(path):
 
 
 ####################################
+# main function
+####################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-precision", help="Precision (value of m)", type=int, default=7)
-    parser.add_argument("-timeout", help="timeout", type=int, default=300)
-    parser.add_argument("-runIndex", help="runIndex", type=int, default=1)
-    parser.add_argument(
-        "inputFile", help="input File (in Weighted CNF format)")
+    parser.add_argument("--precision", help="Precision (value of m)", type=int, default=7)
+    parser.add_argument("--runindex", help="run index", type=int, default=1)
+    parser.add_argument("inputFile", help="input File (in Weighted CNF format)")
+    parser.add_argument("outputFile", help="output File (in Weighted CNF format)")
     args = parser.parse_args()
-    precision = args.precision
-    timeout = args.timeout
-    runIndex = args.runIndex
-    inputFile = args.inputFile
-    tmpDir = os.getenv("TMPDIR", "temp")
 
-    # initialization
-    ensureDirectory(tmpDir+'/')
-    ensureDirectory('Logs/')
-
-    initialFileSuffix = inputFile.split('/')[-1][:-4]
-    outputFile = tmpDir+'/'+str(initialFileSuffix)+'_'+str(runIndex)+'.txt'
-    logFile = 'Logs/'+str(initialFileSuffix)+'_'+str(runIndex)+'.txt'
-    outCNFFile = tmpDir+'/'+str(initialFileSuffix)+'_'+str(runIndex)+'.cnf'
-    tempCNFFile = tmpDir+'/'+str(initialFileSuffix) + \
-        '_temp_'+str(runIndex)+'.cnf'
     startTime = time.time()
     wtVars, origTotalVars, origTotalClaus, totalVars, totalCount, minWeight, eqWtVars = Transform(
-        inputFile, outCNFFile, tempCNFFile, precision, runIndex)
+        args.inputFile, args.outputFile, args.precision, args.runindex)
+    print("Time to transform:", time.time()-startTime)
 
-    endTime = time.time()
-    writeStr = 'Transform:::'+str(endTime-startTime)+':0\n'
+    ####################################
+    # this is some log -- NOT needed.
+    initialFileSuffix = args.inputFile.split('/')[-1][:-4]
+    logFile = 'Logs/'+str(initialFileSuffix)+'_'+str(args.runindex)+'.txt'
+    writeStr = 'Transform:::'+str(time.time()-startTime)+':0\n'
     writeStr += 'Stats:'+str(wtVars)+':'+str(origTotalVars)+':'+str(
         origTotalClaus)+':'+str(totalVars)+':'+str(totalCount)+':'+str(precision)+':0\n'
     f = open(logFile, 'w')
     f.write(writeStr)
     f.close()
+    exit(0)
+
+    ####################################
+    # BELOW NEEDS TO GO TO A SEPARATE PYTHON CODE
+    ####################################
     cmd = './doalarm -t real ' + \
-        str(timeout)+' ./sharpSAT -q -c 3000 '+str(outCNFFile)+' > '+outputFile
+        str(timeout)+' ./sharpSAT -q -c 3000 '+str(args.outputFile)+' > tmp_timing'
     startTime = time.time()
     os.system(cmd)
-    endTime = time.time()
 
-    writeStr = 'ExactCount:sharpSAT::'+str(endTime-startTime)+':0\n'
+    writeStr = 'ExactCount:sharpSAT::'+str(time.time()-startTime)+':0\n'
     f = open(logFile, 'a')
     f.write(writeStr)
     f.close()
 
     # Reading the output of sharpSAT
-    f = open(outputFile, 'r')
+    f = open("tmp_timing", 'r')
     lines = f.readlines()
     f.close()
     for line in lines:
-        if (line.strip() == ''):
+        if line.strip() == '':
             continue
-        if (line.strip()[0].isdigit()):
+        if line.strip()[0].isdigit():
             unweightedCount = int(line.strip())
             break
     logweightedCount = math.log(unweightedCount, 2) - \
         (eqWtVars+totalVars-origTotalVars)
     print("The log weighted count is "+str(logweightedCount))
-    print("The time taken is:"+str(endTime-startTime))
-    print("The logs are printed in: "+str(logFile))
-
-    cmd = 'rm '+str(outCNFFile)
-    os.system(cmd)
-    cmd = 'rm '+str(outputFile)
-    os.system(cmd)
+    print("The time taken is:", time.time()-startTime)
+    print("The logs are printed in: ", logFile)
