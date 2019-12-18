7ae2b2924c725b9be0f2d72a4e582df94dc3408b
Mate Soos <soos.mate@gmail.com>
2019-12-19 00:10:42 +0100

    Adding minimal testing

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
+APPMC="~/development/sat_solvers/scalmc/build/approxmc"
 
 echo "Doing NO IND"
 ./weightcount.py --prec 4 CNFs/a-no-ind.cnf b.cnf
-~/development/sat_solvers/scalmc/build/approxmc b.cnf --sampleout x --samples 1000 > /dev/null
+./${APPMC} b.cnf --sampleout x --samples 1000 > /dev/null
 cut -d " " -f 1-2 x | sort | uniq -c
 
 echo "-------------"
 echo "Doing IND"
 ./weightcount.py --prec 4 CNFs/a-with-ind.cnf b.cnf
-~/development/sat_solvers/scalmc/build/approxmc b.cnf --sampleout x --samples 1000 > /dev/null
+./${APPMC} --sampleout x --samples 1000 > /dev/null
 cut -d " " -f 1-2 x | sort | uniq -c
 
