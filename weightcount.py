a989fbafd299273278a7ad49079febe136c6fdc0
Mate Soos <soos.mate@gmail.com>
2019-12-18 22:43:16 +0100

    Fixing header + license

--- weightcount.py
+++ weightcount.py
@@ -1,8 +1,30 @@
-# @author Kuldeep S Meel 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 # Copyright (c) 2016 Kuldeep S Meel
-# Please see attached License file for licensing information
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 # If you use this code for experiments, please cite the following paper:
-# "From Weighted to UnWeighted Model Counting", Supratik Chakraborty, Dror Fried, Kuldeep S. Meel, Moshe Y. Vardi
+# "From Weighted to UnWeighted Model Counting"
+# by Supratik Chakraborty, Dror Fried, Kuldeep S. Meel, Moshe Y. Vardi
 # Proc. of IJCAI 2016
 
 import sys
