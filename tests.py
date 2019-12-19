4a0ade8a537dcadae0ce85082259c5429170de71
Mate Soos <soos.mate@gmail.com>
2019-12-19 22:24:15 +0100

    Adding some tests

---
+++ tests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Kuldeep S Meel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import unittest
+from weightcount import Converter
+
+class TestMyMethods(unittest.TestCase):
+    def test_parseWeight(self):
+        c = Converter()
+
+        # 1 of 4 is 0.25
+        self.assertEqual(c.parseWeight(0.25, 10), (1.0, 2))
+
+        # 1 of 8 is 0.125
+        self.assertEqual(c.parseWeight(0.125, 10), (1.0, 3))
+
+        # 3 of 4 is 0.75
+        self.assertEqual(c.parseWeight(0.75, 10), (3.0, 2))
+
+        # 3 of 4 is 0.75 -- just about enough bits here
+        self.assertEqual(c.parseWeight(0.75, 2), (3.0, 2))
+
+        # precision must be at least 2 bits
+        with self.assertRaises(AssertionError):
+            c.parseWeight(0.75, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
