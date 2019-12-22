fcad0d4e851a540cf3e758f079efcad0909f8404
Mate Soos <soos.mate@gmail.com>
2019-12-22 17:50:50 +0100

    Renaming files

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ APPMC="/home/soos/development/sat_solvers/scalmc/build/approxmc"
 echo "Doing NO IND"
 rm -f b.cnf
 rm -f x
-./weightcount.py --prec 4 CNFs/a-no-ind.cnf b.cnf
+./weighted_to_unweighted.py --prec 4 CNFs/a-no-ind.cnf b.cnf
 ${APPMC} b.cnf --sampleout x --samples 1000 > /dev/null
 cut -d " " -f 1-2 x | sort | uniq -c
 
@@ -15,7 +15,7 @@ echo "-------------"
 echo "Doing IND"
 rm -f b.cnf
 rm -f x
-./weightcount.py --prec 4 CNFs/a-with-ind.cnf b.cnf
+./weighted_to_unweighted.py --prec 4 CNFs/a-with-ind.cnf b.cnf
 ${APPMC} b.cnf --sampleout x --samples 1000 > /dev/null
 cut -d " " -f 1-2 x | sort | uniq -c
 
