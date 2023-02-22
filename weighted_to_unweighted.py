2ed619ce4bb563dd1fe8334443c429a888ae0591
Mate Soos <soos.mate@gmail.com>
2023-02-22 20:51:02 +0100

    Adding myself as a contributor

--- weighted_to_unweighted.py
+++ weighted_to_unweighted.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 Kuldeep S Meel
+# Copyright (c) 2016-2020 Kuldeep S Meel, Mate Soos
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
